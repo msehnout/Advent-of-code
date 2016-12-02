a86642e88aec2dc23ef0c01c84c121853db8afed
Martin Sehnoutka <msehnout@redhat.com>
2016-12-02 15:08:09 +0100

    Second part of day 2.

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -66,6 +66,106 @@ impl Keyboard {
 
 }
 
+///     1
+///   2 3 4
+/// 5 6 7 8 9
+///   A B C
+///     D
+struct Keyboard2 {
+    val: char,
+}
+
+impl Default for Keyboard2 {
+    fn default() -> Keyboard2 {
+        Keyboard2 {
+            val: '5',
+        }
+    }
+}
+
+impl Keyboard2 {
+    fn up(&mut self) {
+        self.val = match self.val {
+            '1' => '1',
+            '2' => '2',
+            '3' => '1',
+            '4' => '4',
+            '5' => '5',
+            '6' => '2',
+            '7' => '3',
+            '8' => '4',
+            '9' => '9',
+            'A' => '6',
+            'B' => '7',
+            'C' => '8',
+            'D' => 'B',
+            _ => 'X',
+        }
+    }
+
+    fn down(&mut self) {
+        self.val = match self.val {
+            '1' => '3',
+            '2' => '6',
+            '3' => '7',
+            '4' => '8',
+            '5' => '5',
+            '6' => 'A',
+            '7' => 'B',
+            '8' => 'C',
+            '9' => '9',
+            'A' => 'A',
+            'B' => 'D',
+            'C' => 'C',
+            'D' => 'D',
+            _ => 'X',
+        }
+    }
+
+    fn right(&mut self) {
+        self.val = match self.val {
+            '1' => '1',
+            '2' => '3',
+            '3' => '4',
+            '4' => '4',
+            '5' => '6',
+            '6' => '7',
+            '7' => '8',
+            '8' => '9',
+            '9' => '9',
+            'A' => 'B',
+            'B' => 'C',
+            'C' => 'C',
+            'D' => 'D',
+            _ => 'X',
+        }
+    }
+
+    fn left(&mut self) {
+        self.val = match self.val {
+            '1' => '1',
+            '2' => '2',
+            '3' => '2',
+            '4' => '3',
+            '5' => '5',
+            '6' => '5',
+            '7' => '6',
+            '8' => '7',
+            '9' => '8',
+            'A' => 'A',
+            'B' => 'A',
+            'C' => 'B',
+            'D' => 'D',
+            _ => 'X',
+        }
+    }
+
+    fn get_num(&self) -> char {
+        self.val
+    }
+
+}
+
 fn main() {
     let mut args: Vec<String> = env::args().collect();
     let input: String = if args.len() < 2 {
@@ -77,7 +177,7 @@ fn main() {
     println!("I'm going to read file: {}", input);
     let f = File::open(input).unwrap();
     let file = BufReader::new(&f);
-    let mut key = Keyboard::default();
+    let mut key = Keyboard2::default();
     let mut ret = Vec::new();
     for line in file.lines() {
         for c in line.unwrap().chars() {
