b46013cf70ae293d32348b0d693080b2df09527d
Sehny <sehnoutka.martin@gmail.com>
2016-12-06 09:50:06 +0100

    Day 6 part 1

---
+++ d6/src/main.rs
@@ -0,0 +1,28 @@
+use std::io::{self, Read};
+use std::collections::btree_map::BTreeMap;
+
+fn get_message(input: &[&str]) -> String {
+    let msg_size = input[0].len();
+    let mut counter = (0..msg_size).map(|_| BTreeMap::new()).collect::<Vec<_>>();
+    for line in input {
+        for (num, c) in line.chars().enumerate() {
+            *counter[num].entry(c).or_insert(0) += 1;
+        }
+    }
+    let mut msg = String::new();
+    for i in 0..msg_size {
+        let list_char_num = (&counter[i])
+            .into_iter()
+            .collect::<Vec<(&char, &usize)>>();
+        let (c, _): (&char, &usize) = *list_char_num.iter().max_by_key(|x| x.1).unwrap();
+        msg.push(*c);
+    }
+    msg
+}
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+    let input = buffer.lines().collect::<Vec<&str>>();
+    println!("Message: {}", get_message(&input));
+}
